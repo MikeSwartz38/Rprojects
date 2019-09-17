9ae4e169c67d7d482c939f9831ef763021816b39
mikesw38 <you@example.com>
2019-09-17 14:48:54 -0400

    test

--- spotify-server-JK/spofity-server.js
+++ spotify-server-JK/spofity-server.js
@@ -1,7 +1,7 @@
 /* Load the HTTP library */
 var http = require("http");
 
-/* Create an HTTP server to handle responses */
+/* Create an HTTP server to handle TEST responses */
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
