fb2f18cafb87aff3add4fd5d94c3236e5cb9214b
mikesw38 <you@example.com>
2019-09-18 09:15:38 -0400

    completed creating a simple server-side application that accesses user related data. demonstrates how to retrieve data using an authorization flow; template for application

--- spotify-server-ms/authorization_code/app.js
+++ spotify-server-ms/authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
+var client_id = '06ca36a1996d4462ab8cd36d3f89d4bd'; // Your client id
 var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
