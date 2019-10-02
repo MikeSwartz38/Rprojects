7eedb1f830704ddb7361f731508a3d661ff2a518
mikesw38 <you@example.com>
2019-10-02 14:13:53 -0400

    created objects to simplify

--- game-canvas/game.js
+++ game-canvas/game.js
@@ -15,56 +15,63 @@ ctx.font = '30px Arial';
 const height = 500;
 const width = 500;
 
-//player:
-var x = 50;
-var y = 40;
-var spdX = 30;
-var spdY = 5;
-var name = 'P'
-
-//enemy:
-var enemy_x = 50;
-var enemy_y = 350;
-var enemy_spdX = 30;
-var enemy_spdY = 5;
-var enemy_name = 'E'
-
 var message = 'Bouncing';
 
-ctx.fillText(name, x, y);
+var player = {
+    x: 50,
+    y: 40,
+    spdX: 30,
+    spdY: 5,
+    name: 'P'
+}
+
+var enemy = {
+    x: 50,
+    y: 350,
+    spdX: 30,
+    spdY: 5,
+    name: 'E'
+};
 
 setInterval(update, 40); //(name of function, time period in ms);
 
-function update() {
+function updatePlayer () {
     //player:
-    x += spdX;
-    y += spdY;
-    ctx.fillText(name,x,y)
+    player.x += player.spdX;
+    player.y += player.spdY;
+    ctx.fillText(player.name,player.x,player.y)
 
-    if (x < 0 || x > width) {
+    if (player.x < 0 || player.x > width) {
         console.log(message);
-        spdX = -spdX;
+        player.spdX = -player.spdX;
     }
 
-    if (y < 0 || y > height) {
+    if (player.y < 0 || player.y > height) {
         console.log(message);
-        spdY = -spdY;
+        player.spdY = -player.spdY;
     }
+}
 
-      //enemy:
-      enemy_x += enemy_spdX;
-      enemy_y += enemy_spdY;
-      ctx.fillText(enemy_name,enemy_x,enemy_y)
-  
-      if (enemy_x < 0 || enemy_x > width) {
-          console.log(message);
-          enemy_spdX = -enemy_spdX;
-      }
-  
-      if (enemy_y < 0 || enemy_y > height) {
-          console.log(message);
-          enemy_spdY = -enemy_spdY;
-      }
+function updateEnemy () {
+     //enemy:
+     enemy.x += enemy.spdX;
+     enemy.y += enemy.spdY;
+     ctx.fillText(enemy.name,enemy.x,enemy.y)
+ 
+     if (enemy.x < 0 || enemy.x > width) {
+         console.log(message);
+         enemy.spdX = -enemy.spdX;
+     }
+ 
+     if (enemy.y < 0 || enemy.y > height) {
+         console.log(message);
+         enemy.spdY = -enemy.spdY;
+     }
+}
+
+function update() {
+    updatePlayer();
+    updateEnemy();
 }
 
 
